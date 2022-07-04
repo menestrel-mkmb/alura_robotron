2631d706740a53cc0ca9e30b088d3647ef6f32ee
Michael <michaelkmb@hotmail.com>
2022-07-04 16:49:00 -0300

    feat upgrades update stats;

--- js/main.js
+++ js/main.js
@@ -1,6 +1,49 @@
 (() => {
   const robo = document.querySelector(".robo");
-  let estatiticas = {};
+
+  let estatiticas = {
+    forca: 0,
+    poder: 0,
+    energia: 0,
+    velocidade: 0,
+  };
+
+  const pecas = {
+    braco: {
+      forca: 29,
+      poder: 35,
+      energia: -21,
+      velocidade: -5,
+    },
+
+    blindagem: {
+      forca: 41,
+      poder: 20,
+      energia: 0,
+      velocidade: -20,
+    },
+
+    nucleo: {
+      forca: 0,
+      poder: 7,
+      energia: 48,
+      velocidade: -24,
+    },
+
+    perna: {
+      forca: 27,
+      poder: 21,
+      energia: -32,
+      velocidade: 42,
+    },
+
+    foguete: {
+      forca: 0,
+      poder: 28,
+      energia: 0,
+      velocidade: -2,
+    },
+  };
 
   let getStatistics = (parameter) => {
     const obj = document.querySelector(`[data-statistics=${parameter}]`);
@@ -9,7 +52,7 @@
 
   let setStatistics = (parameter, value) => {
     const obj = document.querySelector(`[data-statistics=${parameter}]`);
-    obj.textContent = value.toString();
+    obj.textContent = (parseInt(obj.textContent, 10) + value).toString();
     updateStatistics();
   };
 
@@ -29,19 +72,21 @@
 
   let setMechaValue = (event) => {
     const types = event.target.dataset;
-    let targ;
-    if (types.minus) {
-      const where = types.minus;
-      targ = document.querySelector(`[data-parte=${where}]`);
-      if (parseInt(targ.value, 10) > 0)
-        targ.value = (parseInt(targ.value) - 1).toString();
-    }
-    if (types.plus) {
-      const where = types.plus;
-      targ = document.querySelector(`[data-parte=${where}]`);
-      if (parseInt(targ.value, 10) >= 0)
-        targ.value = (parseInt(targ.value) + 1).toString();
-    }
+    let targ, where;
+
+    types.minus ? (where = types.minus) : (where = types.plus);
+    targ = document.querySelector(`[data-parte=${where}]`);
+    if (targ.value == 0 && types.minus) return;
+    targ.value = (
+      parseInt(targ.value, 10) + (types.minus ? -1 : +1)
+    ).toString();
+
+    const names = Object.keys(pecas[where]);
+    const numbers = Object.values(pecas[where]);
+    names.forEach((name, index) => {
+      const num = numbers[index];
+      setStatistics(name, types.plus ? num : -num);
+    });
   };
 
   const botoes = getPecas();
