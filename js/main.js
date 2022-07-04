c30a8ea8aba7ee873c75647c71d83b9ea42a16f2
Michael <michaelkmb@hotmail.com>
2022-07-04 14:50:12 -0300

    feat convert statistics to dataset;

--- js/main.js
+++ js/main.js
@@ -2,57 +2,26 @@
   const robo = document.querySelector(".robo");
   let estatiticas = {};
 
-  let getForca = () => {
-    return parseInt(
-      document.querySelector(".estatistica-numero.forca").textContent,
-      10
-    );
+  let getStatistics = (parameter) => {
+    const obj = document.querySelector(`[data-statistics=${parameter}]`);
+    return parseInt(obj.textContent, 10);
   };
 
-  let setForca = (valor) => {
-    document.querySelector(".estatistica-numero.forca").textContent =
-      valor.toString();
+  let setStatistics = (parameter, value) => {
+    const obj = document.querySelector(`[data-statistics=${parameter}]`);
+    obj.textContent = value.toString();
     updateStatistics();
   };
 
-  let getPoder = () => {
-    return parseInt(
-      document.querySelector(".estatistica-numero.poder").textContent,
-      10
-    );
+  let updateStatistics = () => {
+    estatiticas = {
+      forca: getStatistics("forca"),
+      poder: getStatistics("poder"),
+      energia: getStatistics("energia"),
+      velocidade: getStatistics("velocidade"),
+    };
   };
-
-  let setPoder = (valor) => {
-    document.querySelector(".estatistica-numero.poder").textContent =
-      valor.toString();
-    updateStatistics();
-  };
-
-  let getEnergia = () => {
-    return parseInt(
-      document.querySelector(".estatistica-numero.energia").textContent,
-      10
-    );
-  };
-
-  let setEnergia = (valor) => {
-    document.querySelector(".estatistica-numero.energia").textContent =
-      valor.toString();
-    updateStatistics();
-  };
-
-  let getVelocidade = () => {
-    return parseInt(
-      document.querySelector(".estatistica-numero.velocidade").textContent,
-      10
-    );
-  };
-
-  let setVelocidade = (valor) => {
-    document.querySelector(".estatistica-numero.velocidade").textContent =
-      valor.toString();
-    updateStatistics();
-  };
+  updateStatistics();
 
   let getPecas = () => {
     return document.querySelectorAll(".controle-ajuste");
@@ -62,14 +31,12 @@
     const types = event.target.dataset;
     let targ;
     if (types.minus) {
-      const operation = "minus";
       const where = types.minus;
       targ = document.querySelector(`[data-parte=${where}]`);
       if (parseInt(targ.value, 10) > 0)
         targ.value = (parseInt(targ.value) - 1).toString();
     }
     if (types.plus) {
-      const operation = "plus";
       const where = types.plus;
       targ = document.querySelector(`[data-parte=${where}]`);
       if (parseInt(targ.value, 10) >= 0)
@@ -81,14 +48,4 @@
   botoes.forEach((botao) => {
     botao.addEventListener("click", setMechaValue);
   });
-
-  let updateStatistics = () => {
-    estatiticas = {
-      forca: getForca(),
-      poder: getPoder(),
-      energia: getEnergia(),
-      velocidade: getVelocidade(),
-    };
-  };
-  updateStatistics();
 })();
